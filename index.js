223a3342c43f3498f3b70942e7646baf4aef1b65
waleed7333 <waleed733150@gmail.com>
2024-08-19 23:01:16 +0300

    update files

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, query) {
+  return drivers.filter((driver) => driver.startsWith(query));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => driver.name === name);
+}
+
+module.exports = {
+  findMatching,
+  fuzzyMatch,
+  matchName,
+};
